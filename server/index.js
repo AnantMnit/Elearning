1f874d4cf2a05b546335a0c5eac57f9f8f8652d7
Anant Goyal <anantgoyal3009@gmail.com>
2025-03-22 23:12:27 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,32 @@
-import express from 'express'
+import express from "express";
+import { connectDB } from "./database/db.js";
+import dotenv from "dotenv";
 
-const app = express()
 
-const port = 5000;
+dotenv.config();
+
+const app = express();
+
+//using middlewares
+app.use(express.json()); 
+
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
     res.send("Server is Working");
 })
 
+// importing routes
+
+import userRoutes from './routes/user.js'
+
+// using routes
+
+app.use("/api", userRoutes);
+
+
+
 app.listen(port, ()=>{
-    console.log(`Server is running on https://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
+    connectDB()
 })
---
+++ server/routes/user.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { register, verifyUser } from "../controllers/user.js";
+
+const router = express.Router();
+
+router.post("/user/register", register);
+router.post("/user/verify", verifyUser);
+
+export default router;
+ 
